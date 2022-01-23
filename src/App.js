44fb66eddcd4312aaf372975c7d8f0c0349d5fb5
YidaLi-iron <>
2022-01-23 15:43:31 -0800

    hw3

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+      <img src="https://images-platform.99static.com//TegOP6NNSiJl_eenYyYgrtdNvjI=/0x0:1000x1000/fit-in/500x500/projects-files/50/5077/507780/372ed020-e22a-4a27-b37b-d5e47750893c.png" class="logo linear infinite" width="3.5rem" height="3.5rem" />
+        <span className="title">LiveChat!</span>
+      </header>
+      <footer className="footer">
+        <input className="text-input" />
+        <button className="send">↑</button>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
